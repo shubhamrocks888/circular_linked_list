992e10b6c8687860e6b20dadced5541a47b9d6c8
unknown <shubhamrocks888@gmail.com>
2020-06-02 16:33:03 +0530

    push method alternative

--- traversal.py
+++ traversal.py
@@ -21,6 +21,19 @@ class circular_linked_list:
             new_node.next = new_node
         self.head = new_node
 
+##    def push(self,data):
+##        new_node = node(data)
+##        cur_node = self.head
+##        if self.head:
+##            while cur_node.next!=self.head:
+##                cur_node = cur_node.next
+##            new_node.next = self.head
+##            cur_node.next = new_node
+##            self.head = new_node
+##        else:
+##            self.head = new_node
+##            new_node.next = self.head
+
     def print_list(self):
         cur_node = self.head
         while cur_node.next!=self.head:
